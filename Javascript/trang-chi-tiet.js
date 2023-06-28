d3450969a6989eb028cc841f99d414055c6e776c
tkngnn <ngan93048@gmail.com>
2023-06-28 18:13:09 +0700

    253-webtiki

--- Javascript/trang-chi-tiet.js
+++ Javascript/trang-chi-tiet.js
@@ -44,8 +44,54 @@ var product ={
         setInterval(nextBanner, 70000);
     },
 
+    slide_search11: function () {
+        var bannerSize = document.querySelector('.product-ed-1').clientWidth;
+
+        var banner = document.querySelector('.product-ed-full-1');
+
+        var bannerPrev = document.querySelector('.product-pre-1');
+
+        var bannerNext = document.querySelector('.product-next-1');
+
+        var bannerFull = bannerSize * 2;
+
+        bannerFull -= bannerSize;
+
+        var bannerTransfer = 0
+
+        function nextBanner() {
+            if (bannerTransfer < bannerFull) {
+                bannerTransfer += bannerSize;
+            } else {
+                bannerTransfer = 0;
+            }
+
+            banner.style.transform = 'translateX(' + '-' + bannerTransfer + 'px';
+
+        }
+
+        bannerNext.addEventListener('click', nextBanner);
+
+        function prevBanner() {
+            if (bannerTransfer == 0) {
+                bannerTransfer = bannerFull;
+            }
+
+            bannerTransfer -= bannerSize;
+
+            banner.style.transform = 'translateX(' + '-' + bannerTransfer + 'px';
+
+        }
+
+        bannerPrev.addEventListener('click', prevBanner);
+
+
+        setInterval(nextBanner, 70000);
+    },
+
     start: function () {
         this.bannerSliderpro();
+        this.slide_search11();
     }
 };
 product.start();
